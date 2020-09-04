6af26199186eb874cfd53936b5cf52220dd069c5
amanda0617 <amanda21limah@gmail.com>
2020-09-04 23:36:38 +0000

    04/09 Busca Binaria

---
+++ busca-binaria.js
@@ -0,0 +1,49 @@
+// Pré requisito para busca binária: o conjunto de dados
+// PRECISA estar ordenado pelo critério de busca
+function buscaBinaria(lista, valorBusca) {
+    let inicio = 0
+    let fim = lista.length - 1
+
+    while(fim >= inicio){
+        // Math.floor(): retira as casas decimais de um número
+        let meio = Math.floor((fim + inicio) / 2)
+
+        // Verifica se o valor na posição média é o valor de busca
+        if(valorBusca === lista[meio]) {
+            comp++
+            return meio
+        }
+        else if(valorBusca < lista[meio]) {
+            comp += 2
+            fim = meio - 1
+        }
+        else { // ValorBusca > lista[meio]
+            comp += 2
+            inicio = meio + 1
+        }
+    }
+    return -1        // Valor não encontrado
+
+}
+let vetorNomes = require('./dados/vetor-nomes')
+
+console.time('FAUSTO')
+console.log(buscaBinaria(vetorNomes, 'FAUSTO'))
+console.timeEnd('FAUSTO')
+console.log('Comparações: ', comp)
+
+console.log('----------------------------------------')
+
+comp = 0
+console.time('ZOZIMO')
+console.log(buscaBinaria(vetorNomes, 'ZOZIMO'))
+console.timeEnd('ZOZIMO')
+console.log('Comparações: ', comp)
+
+console.log('----------------------------------------')
+
+comp = 0
+console.time('INSTAGRAMILDA')
+console.log(buscaBinaria(vetorNomes, 'INSTAGRAMILDA'))
+console.timeEnd('INSTAGRAMILDA')
+console.log('Comparações: ', comp)
